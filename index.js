5453b56b8b62d5408158cd5362ba2e2eef013767
jgeorgilas <justin.georgilas@gmail.com>
2022-12-22 20:29:45 -0800

    added from class

--- index.js
+++ index.js
@@ -2,4 +2,24 @@ let weeklyExpenses = 0
 let monthlyExpenses = 0
 let annualExpenses = 0
 
-// Your Code Here
+for (let i = 0; i < weeklyExpenseQuestions.length; i++){
+    const stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+    const numberAnswer = parseFloat(stringAnswer)
+    console.log("numberAnswer", numberAnswer, typeof numberAnswer)
+    weeklyExpenses = weeklyExpenses + numberAnswer
+    
+  }
+  for (let i = 0; i < monthlyExpenseQuestions.length; i++){
+    const stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    const numberAnswer = parseFloat(stringAnswer)
+    console.log("numberAnswer", numberAnswer, typeof numberAnswer)
+    monthlyExpenses = monthlyExpenses + numberAnswer
+    
+  }
+  for (let i = 0; i < annualExpenseQuestions.length; i++){
+    const stringAnswer = window.prompt(annualExpenseQuestions[i])
+    const numberAnswer = parseFloat(stringAnswer)
+    console.log("numberAnswer", numberAnswer, typeof numberAnswer)
+    annualExpenses = annualExpenses + numberAnswer
+    
+  }
